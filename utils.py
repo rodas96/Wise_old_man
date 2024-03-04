4bd44e526d8d71603f641da634c33c93f5c1a084
rodas96 <rodriomiguelmorais@gmail.com>
2024-03-04 19:37:37 +0000

    a

--- utils.py
+++ utils.py
@@ -10,14 +10,13 @@ def make_request(url, payload):
 
 
 def find_player_from_overall(soup, overall_rank):
-    # Find all 'td' elements with class 'right' that potentially contain the overall rank
+    # Find all 'td' elements with class 'right' contain overall
     rank_cells = soup.find_all('td', class_='right')
     for rank_cell in rank_cells:
-        # Check if this 'td' contains the overall rank we're looking for
         if rank_cell.text.replace(',', '').strip() == str(overall_rank):
-            next_sibling = rank_cell.find_next_sibling('td', class_='left')
+            next_sibling = rank_cell.find_next_sibling('td', class_='left') #next element td on class has the name of the player
             if next_sibling and next_sibling.a:
-                return next_sibling.a.text.strip().replace('\xa0', ' ') #escape sequence \xa0 and is commonly found in HTML content
+                return next_sibling.a.text.strip().replace('\xa0', ' ') #escape sequence \xa0 somtimes in htlm content
     return None
 
 
